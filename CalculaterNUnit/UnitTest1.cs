a00866038e6de3121f192a566326448515c600c0
Cecilie Lynggaard Jeppesen <au661804@uni.au.dk>
2022-02-03 13:17:33 +0100

    Add project files.

---
+++ CalculaterNUnit/UnitTest1.cs
@@ -0,0 +1,44 @@
+using System.Runtime.InteropServices.ComTypes;
+using NUnit.Framework;
+using Hand_test_Calculator;
+
+namespace CalculaterNUnit
+{
+    public class Tests
+    {
+        private  Calculator uut;
+        
+
+        [SetUp]
+        public void Setup()
+        {
+            uut = new Calculator();
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            
+            double plus = uut.Add(2, 8);
+
+            Assert.That(plus, Is.EqualTo(10));
+        }
+        [Test]
+        public void Test2()
+        {
+
+            double gange = uut.Multiply(2, 8);
+
+            Assert.That(gange, Is.EqualTo(16));
+        }
+        //[TestCase(4,3, ExpectedResult = 12)]
+        //public void Test3(double a, double b)
+        //{
+
+        //    double gange = uut.Multiply(a, b);
+
+            
+        //}
+    }
+}
---
+++ Hand_test_Calculator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Hand_test_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var Calculator = new Calculator();
+
+            double num1 = 3;
+            double num2 = 5;
+
+            var plus = Calculator.Add(num1, num2);
+            var minus = Calculator.Subtract(num1, num2);
+            var gange = Calculator.Multiply(num1, num2);
+            var exp = Calculator.Power(num1, num2);
+
+            Console.WriteLine("Adding " + num1 + " and " + num2);
+            Console.WriteLine("Result: " + plus);
+            Console.WriteLine("Subtracting " + num2 + " from " + num1);
+            Console.WriteLine("Result: " + minus);
+            Console.WriteLine("Multiplying " + num1 + " and " + num2);
+            Console.WriteLine("Result: " + gange);
+            Console.WriteLine( num1 + " to the power of " + num2);
+            Console.WriteLine("Result: " + exp);
+
+
+            Console.ReadKey();
+        }
+    }
+}
